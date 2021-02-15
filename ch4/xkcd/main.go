412c5926219a4eec85b5031541f26ba5032bb388
Kamil <unnamedxaer@gmail.com>
2021-02-15 17:37:04 +0100

    Update

--- ch4/xkcd/main.go
+++ ch4/xkcd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -27,16 +26,16 @@ func (e *notFoundError) Error() string {
 }
 
 type comic struct {
-	Year       int    `json:"year,string"`
-	Month      int    `json:"month,string"`
-	Day        int    `json:"day,string"`
-	Num        int    `json:"num,string"`
+	Year       int `json:"year,string"`
+	Month      int `json:"month,string"`
+	Day        int `json:"day,string"`
+	Num        int
 	SafeTitle  string `json:"safe_title"`
 	Title      string
 	Link       string
 	Transcript string
 	Img        string
-	alt        string
+	Alt        string
 }
 
 var titleFlag *string = flag.String("t", "", "pass comic name")
@@ -58,10 +57,10 @@ func printComic() {
 
 	b, err := json.MarshalIndent(c, "", "    ")
 	if err != nil {
-		fmt.Printf("The comic was found by unable to pretty print. Simple print:\n%v", c)
+		fmt.Printf("The comic was found by unable to pretty print. Simple print:\n%v\n", c)
 		return
 	}
-	fmt.Printf("Your comic is:\n%s", b)
+	fmt.Printf("\nYour comic is:\n%s", b)
 }
 
 func findComicInStorage() (*comic, error) {
@@ -70,9 +69,27 @@ func findComicInStorage() (*comic, error) {
 		return nil, err
 	}
 
-	comic := findComicByTitle(comics, *titleFlag)
+	if *numFlag != 0 {
+		comic := findComicByNum(comics, *numFlag)
+		if comic != nil {
+			return comic, nil
+		}
+	}
 
-	return comic, nil
+	if *titleFlag != "" {
+		comic := findComicByTitle(comics, *titleFlag)
+		return comic, nil
+	}
+	return nil, fmt.Errorf("there is no comic matching to your criteria")
+}
+
+func findComicByNum(comics []comic, num int) *comic {
+	for i := len(comics) - 1; i >= 0; i-- {
+		if comics[i].Num == num {
+			return &comics[i]
+		}
+	}
+	return nil
 }
 
 func findComicByTitle(comics []comic, title string) *comic {
@@ -124,7 +141,7 @@ func checkFlags() error {
 func makeDB() {
 	existed := ensureFileExists(storageFile)
 	if existed == false {
-		fmt.Println("There was not comics storage, the comics have to be downloaded. This may take a few moments...")
+		fmt.Println("There was not comics storage, the comics have to be downloaded. This may take a few moments...\n ")
 		downloadComics()
 	}
 }
@@ -137,24 +154,28 @@ func downloadComics() {
 		panic(err)
 	}
 	defer f.Close()
-
+	err404Cnt := 0
 	for {
-		if n > 5 {
-			fmt.Println("you should now have 5 comics in the storage file")
-			return
-		}
+		// if n > 5 {
+		// 	fmt.Println("you should now have 5 comics in the storage file")
+		// 	return
+		// }
 		b, err := fetchComic(n)
 		if err != nil {
 			if errors.Is(err, &notFoundError{}) {
-				fmt.Printf("comic with num %q not found, programs stops futher downloading\n", n)
-				break
+				fmt.Printf("comic with num %q not found\n", n)
+				err404Cnt++
+				if err404Cnt > 5 {
+					fmt.Print("5 comics not found in a row, programs stops futher downloading\n")
+					break
+				}
 			}
 			failedComics = append(failedComics, n)
 			fmt.Printf("comic number %q added to retires due to error: %q\n", n, err)
 			n++
 			continue
 		}
-
+		err404Cnt = 0
 		err = saveComic(f, b)
 		if err != nil {
 			failedComics = append(failedComics, n)
@@ -182,15 +203,16 @@ func fetchComic(n int) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	fmt.Printf("\n%d.\n%s\n", n, b)
+
 	res.Body.Close()
 	return b, nil
 }
 
 // saveComic appends comic data to storage file
 func saveComic(f *os.File, b []byte) error {
-	fmt.Println(string(b))
-	nl := []byte("\n")
-	b = bytes.ReplaceAll(append(b, '\n'), nl, nil)
+	b = append(b, '\n')
 	_, err := f.Write(b)
 	return err
 }
